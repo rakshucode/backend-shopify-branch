444ae8481923305299663fbd4c8d5d3088f92e3a
Rakshith <rakshithmnayak@gmail.com>
2023-07-08 13:37:52 +0530

    cors added

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 const express = require("express");
+const cors = require("cors");
+
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(cors());
 app.use(express.json());
 
 dotenv.config();
